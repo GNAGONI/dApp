4402063d239ec2941ce93fd7afd324796817c3d1
Nikita Goncharov <nikita.goncharov@itrexgroup.com>
2020-01-14 15:02:13 +0300

    Implement basic contract compilation

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
+    mocha: true,
   },
   extends: ['airbnb-base'],
   globals: {
@@ -15,5 +16,6 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'arrow-parens': 0,
   },
 };
